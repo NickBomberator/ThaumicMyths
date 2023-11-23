44ccdb515f83fc03238316b8fc8d2cd4b6f21ce1
NickBomberator <nickbomberator@gmail.com>
2023-11-23 18:04:09 +0100

    Initial Commit

---
+++ src/main/java/thaumicmyths/ThaumicMyths.java
@@ -0,0 +1,33 @@
+package thaumicmyths;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = ThaumicMyths.MOD_ID, name = ThaumicMyths.NAME, version = ThaumicMyths.VERSION, useMetadata = true)
+public class ThaumicMyths
+{
+  public static final String MOD_ID = "thaumicmyths";
+  public static final String NAME = "Thaumic Myths";
+  public static final String VERSION = "1.0.0-ALPHA";
+
+  @EventHandler
+  public static void preInit(FMLPreInitializationEvent event)
+  {
+
+  }
+
+  @EventHandler
+  public static void init(FMLInitializationEvent event)
+  {
+
+  }
+
+  @EventHandler
+  public static void postInit(FMLPostInitializationEvent event)
+  {
+
+  }
+}
